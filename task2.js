396f9c0b392e94a3ab500ea2996a02640876c54f
Nikolai Syrovezhko <syrovezhko@list.ru>
2022-06-19 23:23:12 +0300

    feat: implement getriginalrice function

--- task2.js
+++ task2.js
@@ -1,4 +1,17 @@
 // черновик
+function getOriginalPrice() {
+  let classArr = document.getElementsByClassName("total")[0].lastChild;
+  let originalPrice = classArr.textContent.split("");
+  let originalPriceNum = [];
+  for(let i = 0; i < originalPrice.length; i++){
+    if (originalPrice[i] !== " " && originalPrice[i] !== "₽") {
+      originalPriceNum.push(originalPrice[i]);
+    }
+  }
+  return originalPriceNum.join("");
+}
+
+
 document.addEventListener('mousedown',  function (event){
   // нахожу способ оплаты "Долями"
   let sharesPayment = document.getElementsByClassName('ctrl')[3].querySelector('input');
@@ -7,7 +20,10 @@ document.addEventListener('mousedown',  function (event){
   // узнаю, кликнул ли я на способ оплаты "Долями"
   if (sharesPayment === check) {
     console.log('yes')
+    console.log(getOriginalPrice())
   } else {
     console.log('no')
   }
 })
+
+console.log(document.getElementsByClassName("total")[0].lastChild.textContent.split(""))
