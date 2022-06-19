e2b4ed7e150ce7b3aab8943695ddd05ebb0629c6
Nikolai Syrovezhko <syrovezhko@list.ru>
2022-06-20 01:06:07 +0300

    fix: delete div if not part_pay option

--- task2.js
+++ task2.js
@@ -29,8 +29,8 @@ function addChild(arr) {
   console.log("parent is", parent);
   let content = document.createElement("div");
   content.textContent = `2 недели: ${arr[0]}, 4 недели: ${arr[0]}, 6 недель: ${arr[0]}, 8 недель: ${arr[1]}`;
-  content.className = "price";
-  parent.prepend(content);
+  content.className = "price price-custom";
+  parent.after(content);
 }
 
 // TODO: доделать 
@@ -42,6 +42,7 @@ function addChild(arr) {
 //   }
 // }
 
+
 document.addEventListener('mousedown',  function (event){
   // нахожу способ оплаты "Долями"
   let sharesPayment = document.getElementsByClassName('ctrl')[3].querySelector('input');
@@ -49,17 +50,31 @@ document.addEventListener('mousedown',  function (event){
   let check = event.composedPath()[4].getElementsByTagName('input')[0];
   // узнаю, кликнул ли я на способ оплаты "Долями"
   if (sharesPayment === check) {
+    let toRemove = document.getElementsByClassName("price-custom")[0]
+    let isToRemove = toRemove ? true : false;
+    if (isToRemove) {
+      toRemove.remove()
+      console.log("quarterPrice has been removed")
+    }
     console.log('yes');
-    // console.log(getOriginalPrice())
-    // console.log(addChild(makeQuarterPrice( getOriginalPrice())))
     originalPrice = getOriginalPrice();
     quarterPrice = makeQuarterPrice(originalPrice);
-    console.log("originalPrice was:", originalPrice);
-    console.log("quarterPrice is:", quarterPrice)
-    addChild(quarterPrice);
+    // console.log("originalPrice was:", originalPrice);
+    // console.log("quarterPrice is:", quarterPrice)
+    setTimeout(() => addChild(quarterPrice), 7000);
+    // addChild(quarterPrice);
   } else {
+    let toRemove = document.getElementsByClassName("price-custom")[0];
+    let isToRemove = toRemove ? true : false;
+    if (isToRemove) {
+      toRemove.remove()
+      console.log("quarterPrice has been removed")
+    }
     console.log('no')
   }
 })
 
-console.log(document.getElementsByClassName("total")[0].lastChild.textContent.split(""))
+console.log(document.getElementsByClassName("total")[0].lastChild.textContent.split(""));
+let isToRemove = document.getElementsByClassName("price-custom")[0] ? true : false
+console.log(isToRemove)
+console.log('otherPayment.includes(check))', otherPayment.includes(check))
