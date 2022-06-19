d6b75177359a9c3a451f6a263c6d09bc5ba1064e
Nikolai Syrovezhko <syrovezhko@list.ru>
2022-06-19 22:55:44 +0300

    feat: comments for ditection of click to parts pay method

--- task2.js
+++ task2.js
@@ -1,8 +1,10 @@
 // черновик
 document.addEventListener('mousedown',  function (event){
+  // нахожу способ оплаты "Долями"
   let sharesPayment = document.getElementsByClassName('ctrl')[3].querySelector('input');
-
+  // сохраняю элемент, на который нажал
   let check = event.composedPath()[4].getElementsByTagName('input')[0];
+  // узнаю, кликнул ли я на способ оплаты "Долями"
   if (sharesPayment === check) {
     console.log('yes')
   } else {
