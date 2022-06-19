3b2d3bd3663364a009361e79dc035c68ed98cc66
Nikolai Syrovezhko <syrovezhko@list.ru>
2022-06-20 01:40:51 +0300

    feat: isCheckout function

--- task2.js
+++ task2.js
@@ -1,4 +1,3 @@
-// черновик
 function getOriginalPrice() {
   let classArr = document.getElementsByClassName("total")[0].lastChild;
   let originalPrice = classArr.textContent.split("");
@@ -26,55 +25,50 @@ function makeQuarterPrice(originalPrice) {
 
 function addChild(arr) {
   let parent = document.getElementsByClassName('ctrl')[3];
-  console.log("parent is", parent);
   let content = document.createElement("div");
   content.textContent = `2 недели: ${arr[0]}, 4 недели: ${arr[0]}, 6 недель: ${arr[0]}, 8 недель: ${arr[1]}`;
   content.className = "price price-custom";
   parent.after(content);
 }
 
-// TODO: доделать 
-// function isCheckout (originalPrice) {
-//   if (document.getElementsByTagName("body")[0].className.split("-")[0] === "product"){
-//     return isPrice (originalPrice);
-//   } else {
-//     return false;
-//   }
-// }
-
-
-document.addEventListener('mousedown',  function (event){
-  // нахожу способ оплаты "Долями"
-  let sharesPayment = document.getElementsByClassName('ctrl')[3].querySelector('input');
-  // сохраняю элемент, на который нажал
-  let check = event.composedPath()[4].getElementsByTagName('input')[0];
-  // узнаю, кликнул ли я на способ оплаты "Долями"
-  if (sharesPayment === check) {
-    let toRemove = document.getElementsByClassName("price-custom")[0]
-    let isToRemove = toRemove ? true : false;
-    if (isToRemove) {
-      toRemove.remove()
-      console.log("quarterPrice has been removed")
-    }
-    console.log('yes');
-    originalPrice = getOriginalPrice();
-    quarterPrice = makeQuarterPrice(originalPrice);
-    // console.log("originalPrice was:", originalPrice);
-    // console.log("quarterPrice is:", quarterPrice)
-    setTimeout(() => addChild(quarterPrice), 7000);
-    // addChild(quarterPrice);
+function isCheckout() {
+  if (document.getElementsByTagName("h1")[0].textContent === "Оформление заказа"){
+    return true;
   } else {
-    let toRemove = document.getElementsByClassName("price-custom")[0];
-    let isToRemove = toRemove ? true : false;
-    if (isToRemove) {
-      toRemove.remove()
-      console.log("quarterPrice has been removed")
-    }
-    console.log('no')
+    return false;
   }
-})
+}
 
-console.log(document.getElementsByClassName("total")[0].lastChild.textContent.split(""));
-let isToRemove = document.getElementsByClassName("price-custom")[0] ? true : false
-console.log(isToRemove)
-console.log('otherPayment.includes(check))', otherPayment.includes(check))
+// проверяю  checkout ли это
+if(isCheckout()) {
+  document.addEventListener('mousedown',  function (event){
+    // нахожу способ оплаты "Долями"
+    let sharesPayment = document.getElementsByClassName('ctrl')[3].querySelector('input');
+    // сохраняю элемент, на который нажал
+    let check = event.composedPath()[4].getElementsByTagName('input')[0];
+    // узнаю, кликнул ли я на способ оплаты "Долями"
+    if (sharesPayment === check) {
+      // сохраняю полную стоимость
+      originalPrice = getOriginalPrice();
+      // делю стоимость на 4 части
+      quarterPrice = makeQuarterPrice(originalPrice);
+      let toRemove = document.getElementsByClassName("price-custom")[0];
+      let isToRemove = toRemove ? true : false;
+      if (isToRemove) {
+        toRemove.remove();
+        addChild(quarterPrice);
+      } else {
+        // добавляю div через 7 секунд, как раз после перезагрузки страницы
+        setTimeout(() => addChild(quarterPrice), 7000);
+      }
+    } else {
+      let toRemove = document.getElementsByClassName("price-custom")[0];
+      let isToRemove = toRemove ? true : false;
+      if (isToRemove) {
+        toRemove.remove();
+        console.log("quarterPrice has been removed");
+      }
+      console.log('not a shares payment')
+    }
+  })
+}
