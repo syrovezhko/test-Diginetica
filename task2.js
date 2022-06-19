830a5d92303d4a3f3706157a8b50599e39649477
Nikolai Syrovezhko <syrovezhko@list.ru>
2022-06-19 22:41:40 +0300

    feat: can ditect click to parts pay method

--- task2.js
+++ task2.js
@@ -1,26 +1,11 @@
 // черновик
-// let sharesPayment = document.getElementsByClassName('ctrl')[3].querySelector('input');
-
-while (document.getElementsByTagName('h1')[0].textContent === 'Оформление заказа'){
-  let sharesPayment = document.getElementsByClassName('ctrl')[3].querySelector('input');
-}
-let sharesPayment = document.getElementsByClassName('ctrl')[3].querySelector('input');
-console.log(sharesPayment)
 document.addEventListener('mousedown',  function (event){
-  // let sharesPayment = document.getElementsByClassName('ctrl')[3].querySelector('input');
+  let sharesPayment = document.getElementsByClassName('ctrl')[3].querySelector('input');
 
-  // let check = event.path[0]
-  // console.log(event.path[0])
-  // console.log("check")
-  // console.log(check[0])
-  // console.log("sharesPayment")
-  // console.log(sharesPayment)
-  if (sharesPayment in event.path[0]) {
+  let check = event.composedPath()[4].getElementsByTagName('input')[0];
+  if (sharesPayment === check) {
     console.log('yes')
   } else {
     console.log('no')
   }
 })
-
-
-console.log( document.getElementsByTagName('h1')[0].textContent)
